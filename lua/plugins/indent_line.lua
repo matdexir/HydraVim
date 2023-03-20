0a31eb40a44a8cb94b786a48ca0d7e1302026cc9
Diogo-ss <ssdiogo@proton.me>
2023-03-20 20:55:57 -0300

    fix: ident

--- lua/plugins/indent_line.lua
+++ lua/plugins/indent_line.lua
@@ -5,7 +5,21 @@ if not present then
 end
 
 indent_blankline.setup {
+  filetype_exclude = {
+    "terminal",
+    "lazy",
+    "lspinfo",
+    "TelescopePrompt",
+    "help",
+    "TelescopeResults",
+    "mason",
+    "NVimTree",
+    "alpha",
+    "",
+  },
+    buftype_exclude = { "terminal" },
     space_char_blankline = " ",
     show_current_context = true,
     show_current_context_start = false,
+    show_trailing_blankline_indent = false,
 }
